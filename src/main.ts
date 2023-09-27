e9f28ad8b6f7761699cd84ba1b26f40f953438ed
Naskri <naskri.kamil@wp.pl>
2023-09-27 18:59:57 +0200

    Work with TS
    Add a slider functionality with UI changes.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { handleModalFunctionality } from "./modal";
+import { handleSliderFunctionality } from "./slider";
 import "./style/style.css";
 import { handleMenuToggleFunctionality } from "./toggleMenu";
 
 handleMenuToggleFunctionality();
 handleModalFunctionality();
+handleSliderFunctionality();
---
+++ src/slider.ts
@@ -0,0 +1,85 @@
+export const handleSliderFunctionality = () => {
+  const mainImageContainers = document.querySelectorAll(
+    "[data-mainImageContainer]"
+  );
+
+  const allPossibleImagesButtons = document.querySelectorAll(
+    "[data-buttonImages]"
+  );
+
+  const previousSlideButtons = document.querySelectorAll(
+    "[data-previousSlide]"
+  );
+  const nextSlideButtons = document.querySelectorAll("[data-nextSlide]");
+
+  let currentSlide = 1;
+
+  const setCurrentImageUI = () => {
+    removeActiveClassFromImageContainers();
+    const currentThumbnailImageContainers = document.querySelectorAll(
+      `[data-buttonImages="${currentSlide}"]`
+    );
+
+    currentThumbnailImageContainers.forEach(
+      (currentThumbnailImageContainer) => {
+        currentThumbnailImageContainer?.classList.add("active");
+      }
+    );
+    setNewMainImage(currentSlide);
+  };
+
+  const setNewSlide = (newImageID: number) => {
+    currentSlide = newImageID;
+  };
+
+  const previousSlide = () => {
+    if (currentSlide === 1) return;
+    currentSlide--;
+    setCurrentImageUI();
+  };
+
+  const nextSlide = () => {
+    if (currentSlide === allPossibleImagesButtons.length / 2) return;
+    currentSlide++;
+    setCurrentImageUI();
+  };
+
+  const removeActiveClassFromImageContainers = () => {
+    allPossibleImagesButtons.forEach((buttonContainer) =>
+      buttonContainer?.classList.remove("active")
+    );
+  };
+
+  const setNewMainImage = (id: number) => {
+    const FOLDER_URL = "./src/images";
+    const newImageSource = `${FOLDER_URL}/image-product-${id}.jpg`;
+
+    mainImageContainers.forEach((mainImageContainer) => {
+      const mainImage = mainImageContainer?.querySelector(".gallery__image");
+      mainImage?.setAttribute("src", newImageSource);
+    });
+  };
+
+  allPossibleImagesButtons.forEach((buttonContainer) =>
+    buttonContainer?.addEventListener("click", (ev) => {
+      if (!(ev.target instanceof HTMLButtonElement)) return;
+
+      const newMainImageID = Number(ev.target.dataset.buttonimages);
+
+      if (!newMainImageID) return;
+
+      setNewSlide(newMainImageID);
+      setCurrentImageUI();
+    })
+  );
+
+  previousSlideButtons.forEach((previousSlideButton) => {
+    previousSlideButton?.addEventListener("click", previousSlide);
+  });
+
+  nextSlideButtons.forEach((nextSlideButton) => {
+    nextSlideButton?.addEventListener("click", nextSlide);
+  });
+
+  setCurrentImageUI();
+};
